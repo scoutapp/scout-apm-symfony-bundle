8e7d5fb70e5d165490faaef785aafeb2a178e6fb
James Titcumb <james@asgrim.com>
2019-12-12 12:39:13 +0000

    Added a span for the controller event

--- EventListener/InstrumentationListener.php
+++ EventListener/InstrumentationListener.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Scoutapm\ScoutApmBundle\EventListener;
 
+use Scoutapm\Events\Span\Span;
 use Scoutapm\ScoutApmAgent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\Event\TerminateEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
@@ -27,6 +30,16 @@ final class InstrumentationListener implements EventSubscriberInterface
         $this->agent->connect();
     }
 
+    public function onKernelController(ControllerEvent $controllerEvent)
+    {
+        $this->agent->startSpan(Span::INSTRUMENT_CONTROLLER . '/eh'); // @todo determine controller name
+    }
+
+    public function onKernelResponse(ResponseEvent $responseEvent)
+    {
+        $this->agent->stopSpan();
+    }
+
     /** @throws \Exception */
     public function onKernelTerminate(TerminateEvent $terminateEvent)
     {
@@ -40,7 +53,9 @@ final class InstrumentationListener implements EventSubscriberInterface
     {
         return [
             KernelEvents::REQUEST => ['onKernelRequest', -100],
-            KernelEvents::TERMINATE => ['onKernelTerminate', -1024],
+            KernelEvents::CONTROLLER => ['onKernelController', -100],
+            KernelEvents::RESPONSE => ['onKernelResponse', 0],
+            KernelEvents::TERMINATE => ['onKernelTerminate', 0],
         ];
     }
 }
