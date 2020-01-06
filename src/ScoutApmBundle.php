afcd35ed20c22dce1bf34a8fea5cb270685cf7c0
James Titcumb <james@asgrim.com>
2020-01-06 16:41:42 +0000

    Loop through any listed connections to register SQL logger

--- src/ScoutApmBundle.php
+++ src/ScoutApmBundle.php
@@ -7,20 +7,29 @@ namespace Scoutapm\ScoutApmBundle;
 use Doctrine\DBAL\Connection;
 use Scoutapm\ScoutApmBundle\EventListener\DoctrineSqlLogger;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use function array_map;
 
 final class ScoutApmBundle extends Bundle
 {
+    private const DOCTRINE_CONNECTIONS = ['doctrine.dbal.default_connection'];
+
     public function boot() : void
     {
-        if (! $this->container->has('doctrine.dbal.default_connection')) {
-            return;
-        }
+        /** @noinspection UnusedFunctionResultInspection */
+        array_map(
+            function (string $connectionServiceName) : void {
+                if (! $this->container->has($connectionServiceName)) {
+                    return;
+                }
 
-        /** @var DoctrineSqlLogger $sqlLogger */
-        $sqlLogger = $this->container->get(DoctrineSqlLogger::class);
-        /** @var Connection $connection */
-        $connection = $this->container->get('doctrine.dbal.default_connection');
+                /** @var DoctrineSqlLogger $sqlLogger */
+                $sqlLogger = $this->container->get(DoctrineSqlLogger::class);
+                /** @var Connection $connection */
+                $connection = $this->container->get($connectionServiceName);
 
-        $sqlLogger->registerWith($connection);
+                $sqlLogger->registerWith($connection);
+            },
+            self::DOCTRINE_CONNECTIONS
+        );
     }
 }
